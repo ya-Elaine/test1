2543d84a9d3ba923569e338738931b8e1c6f1f09
ya-Elaine <ya-Elaine@tarena>
2019-05-30 15:43:51 +0800

    test3

--- test3/src/test3/Test1.java
+++ test3/src/test3/Test1.java
@@ -2,7 +2,7 @@ package test3;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("test3--Test1");
+		System.out.println("1111");
 	}
 
 }
--- test3/src/test3/Test2.java
+++
@@ -1,8 +0,0 @@
-package test3;
-
-public class Test2 {
-	public static void main(String[] args) {
-		System.out.println("test3--Test2");
-	}
-
-}
